0d1539c52f169a413b26dee142c7afb8f3ea8d52
jeremy <jeremy@jeremys-MBP.hub>
2020-04-22 10:09:24 +0800

    CELO-TEST Temp unit test

--- src/Celo.Service.RandomUser.UnitTests/Service/UserDataProviderTests.cs
+++ src/Celo.Service.RandomUser.UnitTests/Service/UserDataProviderTests.cs
@@ -1,62 +1,71 @@
-using Celo.Data.InMemory;
-using Celo.Service.Models.ServiceModels.Request;
-using Celo.Service.RandomUser.Service;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using NSubstitute;
-using Xunit;
+// using System.Collections.Generic;
+// using System.Linq;
+// using Celo.Data.InMemory;
+// using Celo.Service.Models.ServiceModels.Request;
+// using Celo.Service.RandomUser.Service;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.Extensions.Logging;
+// using NSubstitute;
+// using Xunit;
 
-namespace Celo.Service.RandomUser.UnitTests.Service
-{
-    public class UserDataProviderTests : CoreTest
-    {
+// namespace Celo.Service.RandomUser.UnitTests.Service
+// {
+//     public class UserDataProviderTests : CoreTest
+//     {
 
-        [Fact]
-        public void WhenDeleteAsyncExecuteSuccessThenDeleteSuccessDataOperationReturned()
-        {
+//         [Fact]
+//         public void WhenDeleteAsyncExecuteSuccessThenDeleteSuccessDataOperationReturned()
+//         {
+//             var data = new List<User>
+//             {
+//                 new User { FirstName = "BBB" },
+//                 new User { FirstName = "ZZZ" },
+//                 new User { FirstName = "AAA" },
 
-            // var data = new List<User>
-            // {
-            //     new User { FirstName = "BBB" },
-            //     new User { FirstName = "ZZZ" },
-            //     new User { FirstName = "AAA" },
-            // }.AsQueryable();
+//             }.AsQueryable();
 
-            // var mockSet = Substitute.For<IDbSet<User>>();
-            // mockSet.Provider.Returns(data.Provider);
-            // mockSet.Expression.Returns(data.Expression);
-            // mockSet.ElementType.Returns(data.ElementType);
-            // mockSet.GetEnumerator().Returns(data.GetEnumerator());
+//             ///var mockSet = Substitute.For<DbSet<User>>();
+//             var mockSet = Substitute.For<DbSet<User>, IQueryable<User>>();
 
-            // var mockContext = Substitute.For<UserDataContext>();
-            // mockContext.User.Returns(mockSet);
+//             ((IQueryable<User>) mockSet).Provider.Returns(data.Provider);
+//             ((IQueryable<User>) mockSet).Expression.Returns(data.Expression);
+//             ((IQueryable<User>) mockSet).ElementType.Returns(data.ElementType);
+//             ((IQueryable<User>) mockSet).GetEnumerator().Returns(data.GetEnumerator());
 
-            var logger = Substitute.For<ILogger<UserDataProvider>>();
+//             var mockContext = Substitute.For<UserDataContext>();
+//             mockContext.User.Returns(mockSet);
+
+//             var logger = Substitute.For<ILogger<IUserDataProvider>>();
+
+//             var target = new UserDataProvider(mockContext, logger);
+//             var targetUser = new UserGetRequest {
+
+//             };
+
+//             var result = target.GetUserAsync(targetUser).Result;
+
+//             Assert.True(1==1);
 
 
+//             // var result = target.DeleteUserAsync(fakeUserId).Result;
 
+//             // Assert.True(result == DataOperationStatus.DeleteSuccess);
 
-            // var target = new UserDataProvider(mockService.DataContext, mockService.Logger);
+//         }
 
-            // var result = target.DeleteUserAsync(fakeUserId).Result;
+//         private MockDataProviderObject CreateMockService() => new MockDataProviderObject
+//         {
+//             DataContext = Substitute.For<UserDataContext>(),
+//             Logger = Substitute.For<Logger<UserDataProvider>>()
+//         };
 
-            // Assert.True(result == DataOperationStatus.DeleteSuccess);
+//     }
 
-        }
+//     class MockDataProviderObject
+//     {
 
-        private MockDataProviderObject CreateMockService() => new MockDataProviderObject
-        {
-            DataContext = Substitute.For<UserDataContext>(),
-            Logger = Substitute.For<Logger<UserDataProvider>>()
-        };
+//         public UserDataContext DataContext { get; set; }
 
-    }
-
-    class MockDataProviderObject
-    {
-
-        public UserDataContext DataContext { get; set; }
-
-        public ILogger<UserDataProvider> Logger { get; set; }
-    }
-}
+//         public ILogger<UserDataProvider> Logger { get; set; }
+//     }
+// }
